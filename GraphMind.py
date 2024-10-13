eae7e913ce0d2b82a49a1ef59c84e2ebcdf94bc0
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-13 20:54:39 +0530

    change home

--- GraphMind.py
+++ GraphMind.py
@@ -1,1 +1,16 @@
 import streamlit as st
+
+#app side bar
+with st.sidebar:
+    st.subheader("Upload the manifesto of the candidate.")
+    
+    uploaded_files = st.file_uploader(
+    "Choose a PDF, TXT files", accept_multiple_files=True, type=["pdf", "txt"])
+    
+    
+    for uploaded_file in uploaded_files:
+        st.write("filename:", uploaded_file.name)
+        
+    if uploaded_files:
+        with st.spinner("Processing..."):
+            st.button("Upload to vector store.")
