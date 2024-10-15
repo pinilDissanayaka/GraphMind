26c49c9215f0406ba4819d9007f83621e40e87a1
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-15 10:34:41 +0530

    add connect graph

--- GraphMind.py
+++ GraphMind.py
@@ -14,22 +14,4 @@ with st.sidebar:
     neo4j_password = st.text_input("Neo4j Password", "password", type="password")
     
     if neo4j_url and neo4j_user_name and neo4j_password:
-        col1, col2 = st.columns(2)
-        
-        with col1:
-            st.button("Connect to the Graph.")
-        with col2:
-            st.button("Reset the Graph.")
-    
-    st.button("Connect to Graph.")
-    
-    uploaded_files = st.file_uploader(
-    "Choose a PDF, TXT files", accept_multiple_files=True, type=["pdf", "txt"])
-    
-    
-    for uploaded_file in uploaded_files:
-        st.write("filename:", uploaded_file.name)
-        
-    if uploaded_files:
-        with st.spinner("Processing..."):
-            st.button("Upload to Graph.")
+        st.button("Connect to the Graph.", on_click=connect_graph(url=neo4j_url, password=neo4j_password, userName=neo4j_user_name))
