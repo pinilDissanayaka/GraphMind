fa89dc67c653d0f20162c77fbc89a9e7de9d143c
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-21 14:40:48 +0530

    add temp dir

--- file/file.py
+++ file/file.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from utils.utils import get_embeddings, get_llm
-from langchain.document_loaders import PyPDFLoader, TextLoader, DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_experimental.graph_transformers import LLMGraphTransformer
 
