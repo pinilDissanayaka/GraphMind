6a1adf93500d2654edf51f6564197925aece959d
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-21 08:46:30 +0530

    add secrects

--- secrects/secrets.py
+++ secrects/secrets.py
@@ -3,12 +3,12 @@ import streamlit as st
 
 def setup_secrets(neo4j_url=None, neo4j_user_name=None, neo4j_password=None, groq_api_key=None):
     if neo4j_url or neo4j_user_name or neo4j_password:
-        os.environ["neo4j_url"] = st.secrets["neo4j_url"]
-        os.environ["neo4j_user_name"] = st.secrets["neo4j_user_name"]
-        os.environ["neo4j_password"] = st.secrets["neo4j_password"]
-        os.environ["groq_api_key"] = st.secrets["groq_api_key"]
+        os.environ["NEO4J_URL"] = st.secrets["NEO4J_URL"]
+        os.environ["NEO4J_USER_NAME"] = st.secrets["NEO4J_USER_NAME"]
+        os.environ["NEO4J_PASSWORD"] = st.secrets["neo4j_password"]
+        os.environ["GROQ_API_KEY"] = st.secrets["groq_api_key"]
     else:
-        os.environ["neo4j_url"] = neo4j_url
-        os.environ["neo4j_user_name"] = neo4j_user_name
-        os.environ["neo4j_password"] = neo4j_password
-        os.environ["groq_api_key"] = groq_api_key
+        os.environ["NEO4J_URL"] = neo4j_url
+        os.environ["NEO4J_USER_NAME"] = neo4j_user_name
+        os.environ["NEO4J_PASSWORD"] = neo4j_password
+        os.environ["GROQ_API_KEY"] = groq_api_key
