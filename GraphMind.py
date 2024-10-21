5a039c19e4d7c51f5a9738add7199c120e2c4d99
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-21 09:12:12 +0530

    add connect DB

--- GraphMind.py
+++ GraphMind.py
@@ -15,18 +15,11 @@ with st.sidebar:
     neo4j_url = st.text_input("Neo4j URL", "bolt://localhost:7687")
     neo4j_user_name = st.text_input("Neo4j User Name", "neo4j")
     neo4j_password = st.text_input("Neo4j Password", "password", type="password")
-    
-    st.write("or")
-    
-    uploaded_credentials = st.file_uploader("Upload Credentials", type="txt")
-    if uploaded_credentials:
-        st.write(uploaded_credentials.read())
-    
-    
-    
+
     if neo4j_url and neo4j_user_name and neo4j_password:
-        setup_secrets(neo4j_url, neo4j_user_name, neo4j_password)
-        st.success("✅ Neo4j credentials saved")
+        if st.button("Connect to Neo4j"):
+            graph = connect_graph(neo4j_url, neo4j_user_name, neo4j_password)
+            st.success("✅ Connected to Neo4j")
     else:
         setup_secrets()
         st.success("✅ Neo4j credentials saved")
