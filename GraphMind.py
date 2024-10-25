27d39a21e5ac50cb257d5903c4603f281dcd3723
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-26 01:31:08 +0530

    add home image

--- GraphMind.py
+++ GraphMind.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from graph import connect_graph, create_graph, clear_graph
+from graph import connect_graph, clear_graph
 from file import load_uploaded_file, save_uploaded_file, transform_documents_to_graph_documents, split_documents
 from secrects import setup_secrets
 from tempfile import TemporaryDirectory
